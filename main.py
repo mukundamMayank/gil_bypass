d06ba4e7d0764bd5e6368bc35d234c6ef2ac24ef
mukundamMayank <89220287+mukundamMayank@users.noreply.github.com>
2024-03-24 13:11:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ import ctypes
 import threading
 import time
 
-# Load the shared library
 fibonacci_ext = ctypes.CDLL('./hello.so')
 
-
-# Define the argument and return types for the fibonacci_with_gil and fibonacci_without_gil functions
 fibonacci_ext.fibonacci_without_gil.argtypes = [ctypes.c_int]
 fibonacci_ext.fibonacci_without_gil.restype = ctypes.py_object
 fibonacci_ext.fibonacci_with_gil.argtypes = [ctypes.c_int]
@@ -27,17 +24,14 @@ def calculate_fibonacci_without_gil():
     print(" ########  exit ")
     print("Fibonacci without GIL execution time:", end_time - start_time, "seconds")
 
-# Create two threads for each calculation
 threads = []
 for _ in range(2):
     thread_with_gil = threading.Thread(target=calculate_fibonacci_with_gil)
     thread_without_gil = threading.Thread(target=calculate_fibonacci_without_gil)
     threads.extend([thread_with_gil, thread_without_gil])
 
-# Start all threads
 for thread in threads:
     thread.start()
 
-# Wait for all threads to finish
 for thread in threads:
     thread.join()
